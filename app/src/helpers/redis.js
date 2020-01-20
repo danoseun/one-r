5ed030f6547e0073439cecc103d17b35d407acbf
Abdulsemiu Atanda <semiu@live.com>
2020-01-20 16:23:07 +0100

    add get method to redis tools

--- app/src/helpers/redis.js
+++ app/src/helpers/redis.js
@@ -1,5 +1,9 @@
 import redis from 'redis'
 
-export const redisClient = () => redis.createClient()
+require('dotenv').config()
+
+export const redisClient = () => redis.createClient(process.env.REDIS_URL)
 
 export const setToRedis = (key, value, expiry = 86400) => redisClient().set(key, value, 'EX', expiry)
+
+export const getFromRedis = key => redisClient().get(key, redis.print)
