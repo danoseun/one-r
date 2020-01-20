b597b7b36ac12bf03dfa91994d3f9f7955bfc028
Abdulsemiu Atanda <semiu@live.com>
2020-01-20 13:26:51 +0100

    add some more helpers

--- app/src/helpers/tools.js
+++ app/src/helpers/tools.js
@@ -1,1 +1,9 @@
+import crypto from 'crypto'
+
 export const dateToISOString = date => (new Date(date)).toISOString()
+
+export const isConfirmed = user => user.status === 'confirmed'
+
+export const secureRandom = size => crypto.randomBytes(size).toString('hex')
+
+export const tokenPayload = ({id, firstName, firm_id, role_id}) => ({id, firstName, firm_id, role_id})
