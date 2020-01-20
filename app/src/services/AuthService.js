90668293c36869dfc1c82094d874100ee6507f9b
Abdulsemiu Atanda <semiu@live.com>
2020-01-20 17:43:37 +0100

    add account confirmation method to auth service

--- app/src/services/AuthService.js
+++ app/src/services/AuthService.js
@@ -4,7 +4,16 @@ import bcrypt from 'bcrypt'
 import DataService from './DataService'
 import db from '../../db/models'
 import {generateJWTToken} from '../helpers/authTools'
-import {isConfirmed, tokenPayload, isEmailValid, addRoleToUser, formatRecord, sanitizeUserAttributes, emailDomain} from '../helpers/tools'
+import {
+  isConfirmed,
+  tokenPayload,
+  isEmailValid,
+  addRoleToUser,
+  formatRecord,
+  sanitizeUserAttributes,
+  emailDomain,
+  isConfirmationTokenActive
+} from '../helpers/tools'
 
 require('dotenv').config()
 
@@ -69,6 +78,36 @@ class AuthService {
       console.log('-'.repeat(80))
     })
   }
+
+  confirmAccount(payload) {
+    const {email = ''} = payload
+
+    if (email) {
+      // Check that admin knows the project secret before confirming their account
+      if (payload.token === process.env.SECRET) {
+        return this.data.show({email})
+          .then(user => this.confirmUser(user))
+          .then(updatedUser => ({token: generateJWTToken(tokenPayload(updatedUser)), user: updatedUser}))
+      } else { throw new Error('Unable to confirm account') }
+    } else {
+      const tokenObject = new DataService(db.Token)
+
+      return tokenObject.show({value: payload.token}).then(token => {
+        if (token) {
+          if (isConfirmationTokenActive(token)) {
+            return token.getUser()
+              .then(user => this.confirmUser(user))
+              .then(updatedUser => ({
+                token: generateJWTToken(tokenPayload(formatRecord(updatedUser))),
+                user: updatedUser
+              }))
+          } else { return token.destroy() }
+        } else { throw new Error('Invalid token.')}
+      })
+    }
+  }
+
+  confirmUser(user) { return user.update({status: 'confirmed'}) }
 }
 
 export default AuthService
