e5d7a4390e16baa8e4ae39d476063c5dedd97d26
Abdulsemiu Atanda <semiu@live.com>
2020-01-21 23:02:41 +0100

    wire up template route

--- app/src/routes/firms.js
+++ app/src/routes/firms.js
@@ -3,11 +3,19 @@ import express from 'express'
 
 import firmsController from '../controllers/firms'
 import permissions from '../middlewares/permissions'
+import tempalateController from '../controllers/firms/templates'
 
 const firms = express.Router()
 
 firms.route('/')
   .get(firmsController.index)
   .post(permissions.isAdmin, firmsController.create)
+
+firms.route('/templates')
+  .post(permissions.isManager, tempalateController.create)
+  .get(permissions.isAuthenticated, tempalateController.index)
+
+firms.route('/templates/:id')
+  .patch(permissions.isManager, tempalateController.update)
 
 export default firms
