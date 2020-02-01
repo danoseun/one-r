da982de731001e3ebb456836c50e05d33d33a493
Abdulsemiu Atanda <semiu@live.com>
2020-02-01 18:20:48 +0100

    fix error in seeder file

--- app/db/seeders/20200120111442-roles.js
+++ app/db/seeders/20200120111442-roles.js
@@ -1,13 +1,11 @@
 import uuidV4 from 'uuid/v4'
 
-import {dateToISOString}  from '../../src/helpers/tools'
-
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('Roles', [
-      {id: uuidV4(), name: 'admin', createdAt: dateToISOString(Date.now()), updatedAt: dateToISOString(Date.now())},
-      {id: uuidV4(), name: 'agent', createdAt: dateToISOString(Date.now()), updatedAt: dateToISOString(Date.now())},
-      {id: uuidV4(), name: 'manager', createdAt: dateToISOString(Date.now()), updatedAt: dateToISOString(Date.now())}
+      {id: uuidV4(), name: 'admin', createdAt: (new Date(Date.now())).toISOString(), updatedAt: (new Date(Date.now())).toISOString()},
+      {id: uuidV4(), name: 'agent', createdAt: (new Date(Date.now())).toISOString(), updatedAt: (new Date(Date.now())).toISOString()},
+      {id: uuidV4(), name: 'manager', createdAt: (new Date(Date.now())).toISOString(), updatedAt: (new Date(Date.now())).toISOString()}
     ], {});
   },
   down: (queryInterface, Sequelize) => {
