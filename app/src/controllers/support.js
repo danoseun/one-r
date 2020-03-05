097e52a85e107c1d073e4a8e1127b64fcab2a49e
Abdulsemiu Atanda <semiu@live.com>
2020-03-04 21:23:56 -0500

    fix bug from response change

--- app/src/controllers/support.js
+++ app/src/controllers/support.js
@@ -9,7 +9,7 @@ const support = {
     axios.get(
       `${process.env.CARS_API_BASE_URL}/index.php?route=api/product/getproduct&sku=${req.query.sku}`,
       {headers: {apikey: process.env.CARS_API_KEY}}
-    ).then(response => res.status(OK).send({data: addBaseUrlToImages(response.data[0]), message: null, success: true}))
+    ).then(response => res.status(OK).send({data: addBaseUrlToImages(response.data['0']), message: null, success: true}))
       .catch(() => res.status(UNPROCESSABLE_ENTITY).send({data: null, message: 'SKU might be missing'}))
   }
 }
