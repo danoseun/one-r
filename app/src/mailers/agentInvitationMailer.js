36caf576253d611ccbe28f291ab2950631c8e056
Abdulsemiu Atanda <semiu@live.com>
2020-03-07 09:43:19 -0500

    add agent inivtation mailer

---
+++ app/src/mailers/agentInvitationMailer.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+
+const agentIinvitationHtml = (inviterName, token) => (
+  `
+    <body>
+      <p>
+        ${inviterName} has invited you to join the team on Cars45 Whatsapp Support Solution.
+        Join your team to support and engage your customers via Whatsapp.
+      </p>
+      <a href='${process.env.WEB_HOST}/auth/confirmation/${token}'>Click Here To Join</a>
+    </body>
+  `
+)
+
+const agentInvitationText = (inviterName, token) => (
+  `
+    ${inviterName} has invited you to join the team on Cars45 Whatsapp Support Solution.
+    Join your team to support and engage your customers via Whatsapp.
+    Click Here To Join ${process.env.WEB_HOST}/auth/confirmation/${token}
+  `
+)
+
+const agentInvitationMailer = (inviterName, email, token) => ({
+  from: 'whatsapp@cars45.com',
+  to: email,
+  text: agentInvitationText(inviterName, token),
+  html: agentIinvitationHtml(inviterName, token)
+})
+
+export default agentInvitationMailer
